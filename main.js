a94ef3a83faf64b4f940494c91996b6b28e0d6a0
LEONARDO CONTRERAS MARTINEZ <L21051403@saltillo.tecnm.mx>
2023-12-17 03:29:29 -0600

    I think i know what im doing now

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 $(() =>{
     $('#mainContent').html(() =>{
-        
         return `
             <label><input type="checkbox" id="cbox1" value="nombre" /> Nombre </label>
+            <label><input type="checkbox" id="cbox2" value="lastNameF" /> Apellido Paterno </label>
+            <label><input type="checkbox" id="cbox1" value="lastNameM" /> Apellido Materno </label>
+
         `
     });
+
+
+    //This part of the code should execute the resulting script or result, at first im just going to display it as a sql query, then as a json i guess.
+    $('#okButton').click(()=>{
+        $('#resultContainer').append("<p> ola </p>")
+    })
 })
